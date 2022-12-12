7c107cbc157977e39d0e6fa8b6d25972cb4e4b1c
laan71 <lail5198@edu.easj.dk>
2022-12-12 14:20:36 +0100

    Added checkbox and forgotten password link to signIn.html

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ $(function(){
     $("#header").load("header.html");
     $("#footer").load("footer.html");
 });
+
+function checkmark() {
+    var checkBox = document.getElementById("myCheck");
+    var text = document.getElementById("text");
+    if (checkBox.checked == true){
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
